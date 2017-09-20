d1c4494cd9ff347407f36b32bfb471f3886268a6
George Jor <yhjor@me.com>
2017-09-20 11:52:39 +0800

    Init commit

---
+++ nuxt.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  build: {
+    postcss: [require('postcss-cssnext')()]
+  },
+  head: {
+    title: 'George Jor | Software Engineer | Hong Kong',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ],
+    link: [
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: '//rsms.me/interface/interface.css'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/components/icon.min.css'
+      },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          '//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/components/popup.min.css'
+      },
+      {
+        rel: 'icon',
+        type: 'image/jpeg',
+        href: '/profile.jpg'
+      }
+    ]
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="page">
+    <div>
+      <img
+        class="logo"
+        role="presentation"
+        src="/profile.jpg"
+      />
+    </div>
+    <div>
+      <div class="name">
+        George Jor
+      </div>
+      <div class="title">
+        Software Engineer
+      </div>
+      <hr class="divider" />
+      <div class="contact">
+        <a
+          href="mailto:yhjor@me.com"
+          data-tooltip="Email"
+          data-position="top center"
+          data-inverted
+        >
+          <i class="mail icon" />
+        </a>
+        <span class="separator" />
+        <a
+          href="//github.com/yhjor"
+          target="_blank"
+          data-tooltip="Github"
+          data-position="top center"
+          data-inverted
+        >
+          <i class="github icon" />
+        </a>
+        <span class="separator" />
+        <a
+          href="//www.linkedin.com/in/yhjor"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip="LinkedIn"
+          data-position="top center"
+          data-inverted
+        >
+          <i class="linkedin icon" />
+        </a>
+        <span class="separator" />
+        <a
+           href="//angel.co/yhjor"
+           target="_blank"
+           rel="noopener noreferrer"
+           data-tooltip="AngelList"
+           data-position="top center"
+           data-inverted
+         >
+           <i class="angellist icon" />
+         </a>
+        <span class="separator" />
+        <a
+          href="//twitter.com/yhjor"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip="Twitter"
+          data-position="top center"
+          data-inverted
+        >
+          <i class="twitter icon"/>
+        </a>
+        <span class="separator" />
+        <a
+          href="//medium.com/@yhjor"
+          target="_blank"
+          rel="noopener noreferrer"
+          data-tooltip="Medium"
+          data-position="top center"
+          data-inverted
+        >
+          <i class="medium icon" />
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .page {
+    font-family: Interface, sans-serif;
+
+    min-height: calc(100vh - 16px);
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo {
+    filter: grayscale(90%);
+    border-radius: 50%;
+    width: 140px;
+
+    display: none;
+
+    @media (min-width: 668px) {
+      margin-right: 1.5em;
+      display: block;
+    }
+  }
+
+  .item {
+    padding: 1rem;
+
+    & div {
+      padding: 0.6rem 0;
+    }
+  }
+
+  .name {
+    font-size: 1.5rem;
+    font-weight: bolder;
+    margin-bottom: 0.3em;
+
+    @media (min-width: 668px) {
+      font-size: 2rem;
+    }
+  }
+
+  .title {
+    color: dimgrey;
+    font-size: 1.2rem;
+    margin-bottom: 0.3em;
+
+    @media (min-width: 668px) {
+      font-size: 1.4rem;
+    }
+  }
+
+  .contact {
+    color: dimgrey;
+    font-size: 1.35rem;
+
+    @media (min-width: 668px) {
+      font-size: 1.5rem;
+    }
+
+    & a {
+      text-decoration: none;
+      color: dimgrey;
+    }
+  }
+
+  .divider {
+    margin: 1.2em 0;
+  }
+
+  .separator {
+    padding: 0 0.5em;
+  }
+</style>
